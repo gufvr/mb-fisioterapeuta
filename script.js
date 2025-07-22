3a54c5b35c08fa243b59dbf1ae685cfc9ba1afbc
Gustavo <gu.fvrr@gmail.com>
2025-07-22 19:46:06 -0300

    feat: add favicon-32x32.ico to assets

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function toggleMode() {
   const img = document.querySelector("#profile img");
 
   if (html.classList.contains("light")) {
-    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("src", "./assets/avatar-mari.png");
   } else {
-    img.setAttribute("src", "./assets/avatar.png");
+    img.setAttribute("src", "./assets/avatar-mari.png");
   }
 }
 function toggleMenu() {
